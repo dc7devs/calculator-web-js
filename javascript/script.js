f027f04773721d8b70703cf12ef3f83adb23b73b
Diego Silva <diegocaetano444@outlook.com>
2021-05-15 23:08:05 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -18,19 +18,11 @@ function insert(num){
    let lastValue = input.value.slice(-1);
 
    if (input.value.length == 0 && operator.slice(0, 3).includes(num)) {
-      return 0;
+      return;
 
    } else if (operator.includes(lastValue) && operator.includes(num)) {
       return;
-   }
-   else if (operator.includes(lastValue) && operator.includes(num)) {
-      showNote(`Operação podera vir a ser invalida por conter operadores binário ou mais de um operadores diferente seguidos um após o outro. <br>Tente Ex: <strong>${lastValue}</strong>`);
-      setTimeout(() => {
-         alert.classList.remove('showAlert');
-         alert.removeAttribute("style", "animationName: animacao");
-         alert.innerHTML = "";
-      },10000);
-
+         
    } else if (lastValue == '' && num == '.') {
       input.value += "0";
    }
