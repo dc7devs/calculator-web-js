6f051506d1fdbc51ea3055f4a87f47a772e67b1a
Diego C. Silva <diegocaetano444@outlook.com>
2022-01-03 13:35:58 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -51,6 +51,7 @@ window.addEventListener('keydown', e => {
    // console.log(e.keyCode)
 
    if(e.keyCode == 96 || e.keyCode == 97 || e.keyCode == 98 || e.keyCode == 99 || e.keyCode == 100 || e.keyCode == 101 || e.keyCode == 102 || e.keyCode == 103 || e.keyCode == 104 || e.keyCode == 105 || e.keyCode == 110) {
+      showMessage('<strong style="color: #0f0">Operação em desenvolvimento...<strong>'); return;
       if(e.keyCode == 96) {
 
          if (input.value.length == 1 && input.value.slice(-1) == '0') return;
